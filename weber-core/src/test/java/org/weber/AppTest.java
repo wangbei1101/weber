3b0961df04e556e40c3f3cb837fd9d7829c2d23d
wangbei <wangbei01@renrendai.com>
2015-12-18 15:28:41 +0800

    INIT

---
+++ weber-core/src/test/java/org/weber/AppTest.java
@@ -0,0 +1,16 @@
+package org.weber;
+
+import junit.framework.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        Assert.assertTrue(true);
+    }
+}
